2e752e256e44d63d54b2429fc01a1e337226057f
Malick Laye SY <malicklayesy@gmail.com>
2025-08-07 10:03:17 +0200

    bd config file added

--- key-value-app/start-db.sh
+++ key-value-app/start-db.sh
@@ -7,9 +7,17 @@ ROOT_USER="root-user"
 ROOT_PASSWORD="root-password"
 
 
+#key-value credentiels
+
+KEY_VALUE_DB="key-value-db"
+KEY_VALUE_USER="key-value-user"
+KEY_VALUE_PASSWORD="key-value-password"
 
 docker run --rm -d --name $CONTAINER_NAME \
   -e MONGODB_INITDB_ROOT_USERNAME=$ROOT_USER \
   -e MONGODB_INITDB_ROOT_PASSWORD=$ROOT_PASSWORD \
-
+  -e KEY_VALUE_DB=$KEY_VALUE_DB \
+  -e KEY_VALUE_USER=$KEY_VALUE_USER \
+  -e KEY_VALUE_PASSWORD=$KEY_VALUE_PASSWORD \
+  -v ./db-config/mongo-init.js:/docker-entrypoint-initdb.d/mongo-init.js:ro \
   $MONGODB_IMAGE:$MONGODB_TAG
