9667ad8db9413cbe9c78cd4c1e343803119fc8fd
Malick Laye SY <malicklayesy@gmail.com>
2025-08-20 10:37:02 +0200

    Dockorize the notebooks Servics

--- notes-rest-api/notebooks-backend/src/server.js
+++ notes-rest-api/notebooks-backend/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose= require('mongoose');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 
@@ -10,6 +10,13 @@ app.get('/', (req,res)=>{
    res.json({message :'Hello from notebooks !'}); 
 });
 
-app.listen(port, ()=>{
+mongoose.connect(process.env.DB_URL).then(()=>{
+  console.log('Connect to MongoDB ! Starting server.')
+  app.listen(port, ()=>{
     console.log(`Server listening on port ${port}`);
+});  
+}).catch(err =>{
+    console.error('Something went wront');
+    console.log(err);
 })
+
